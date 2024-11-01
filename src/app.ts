536a68b43034dff9a7483f2799e08554257f1716
Manuel Ulate <fr3zs09@gmail.com>
2024-10-31 20:32:20 -0600

    feat:serving htlm content

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer(( req, res ) => {
 
@@ -18,11 +19,18 @@ const server = http.createServer(( req, res ) => {
   // res.writeHead( 200, { 'Content-Type': 'application/json' } );
   // res.end( JSON.stringify( data ) );
 
+  if(  req.url === '/' ) {
+    const htmlFile = fs.readFileSync( './public/index.html', 'utf-8' );
+    res.writeHead( 200, { 'Content-Type': 'text/html' } );
+    res.end( htmlFile );
+  } else {
+    res.writeHead( 404, { 'Content-Type': 'text/html' } );
+    res.end( '<h1>404 Page Not Found</h1>' );
+  }
+
+
 });
 
 server.listen( 8080, () => {
   console.log('Server is running on port 8080');
 });
-
-console.log('first line');
-
