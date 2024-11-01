6bdcc62b411480bc5c71c29c187cc4d69261a868
Manuel Ulate <fr3zs09@gmail.com>
2024-10-31 20:25:33 -0600

    feat:different type of responses

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,19 @@ const server = http.createServer(( req, res ) => {
 
   console.log( req.url );
 
-  
+  // Server Side rendering of HTML
+  // res.writeHead( 200, { 'Content-Type': 'text/html' } );
+  // res.write('<h1>Hello World</h1>');
+  // res.end();
 
-  res.write('Hello World');
-  res.end();
+  // JSON Response
+  // const data = {
+  //   name: 'John Doe',
+  //   age: 30,
+  //   email: 'jonhdoe@gmail.com'
+  // };
+  // res.writeHead( 200, { 'Content-Type': 'application/json' } );
+  // res.end( JSON.stringify( data ) );
 
 });
 
