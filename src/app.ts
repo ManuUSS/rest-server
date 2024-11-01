557a223860027866a22bc9126871dc55b3e2f81c
Manuel Ulate <fr3zs09@gmail.com>
2024-10-31 20:20:30 -0600

    feat:server opened

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
+import http from 'http';
 
+const server = http.createServer(( req, res ) => {
+
+  console.log( req.url );
+
+  
+
+  res.write('Hello World');
+  res.end();
+
+});
+
+server.listen( 8080, () => {
+  console.log('Server is running on port 8080');
+});
 
 console.log('first line');
 
