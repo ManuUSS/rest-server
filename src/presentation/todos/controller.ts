5e93050e40ed48ad6d4747c58bfcfe201b50b625
Manuel Ulate <fr3zs09@gmail.com>
2024-11-03 18:40:11 -0600

    feat(presentation):todo controller get todo by its id

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,21 +1,37 @@
-import { Request, Response } from 'express';
+import { Application, Request, Response } from 'express';
+
+const todos = [
+  { id: 1, title: 'Todo 1', completed: false },
+  { id: 2, title: 'Todo 2', completed: false },
+  { id: 3, title: 'Todo 3', completed: false },
+];
 
 export class TodosController {
 
 
   constructor () {};
 
-  public getTodos = (req:Request, res:Response) => {
+  public getTodos = ( req:Request, res:Response ) => {
+    res.json( todos );
+  };
 
-    res.json([
-      { id: 1, title: 'Todo 1', completed: false },
-      { id: 2, title: 'Todo 2', completed: false },
-      { id: 3, title: 'Todo 3', completed: false },
-    ]);
+  public getTodoById = ( req:Request, res:Response ) => {
+    const id = Number( req.params.id );
+
+    if( isNaN( id ) ) {
+      res.status( 400 ).json({ message: 'Invalid ID supplied' });
+      return;
+    }
+
+    const todo = todos.find( todo => todo.id === id );
+    
+    todo 
+      ? res.json( todo ) 
+      : res.status( 404 ).json({ message: 'Todo not found' });
 
   };
 
 
-  
+
 
 };
